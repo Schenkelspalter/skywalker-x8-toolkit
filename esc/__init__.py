37902df1a635387c8afee0acfe8aa2aeaf741e1b
Torsten <142719298+Schenkelspalter@users.noreply.github.com>
2026-07-05 17:08:27 +0200

    Add SerialTelemetryReader to __all__ exports

--- esc/__init__.py
+++ esc/__init__.py
@@ -1,4 +1,10 @@
 from .frame import ZTWMantisFrame
+from .reader import SerialTelemetryReader
 from .statistics import RollingFrameStatistics, ValueStats
 
-__all__ = ["ZTWMantisFrame", "RollingFrameStatistics", "ValueStats"]
+__all__ = [
+    "ZTWMantisFrame",
+    "SerialTelemetryReader",
+    "RollingFrameStatistics",
+    "ValueStats",
+]
